29bf1db679d56d0eeb0a052f10a7835dde738e06
M0dius <mohwalalawi@gmail.com>
2024-11-27 13:48:01 +0300

    initial

---
+++ Sustainify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sustainify
+//
+//  Created by Guest User on 27/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
