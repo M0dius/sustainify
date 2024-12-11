cde2682c6e01027763555617f08d77c29624d433
BaderEbrahim <xvbader@gmail.com>
2024-12-11 13:46:37 +0300

    good

--- Sustainify/ProductDetailsViewController.swift
+++ Sustainify/ProductDetailsViewController.swift
@@ -1,13 +1,13 @@
 //
-//  ExpandableTableViewController.swift
+//  ProductDetailsViewController.swift
 //  Sustainify
 //
-//  Created by Guest User on 09/12/2024.
+//  Created by Guest User on 11/12/2024.
 //
 
 import UIKit
 
-class ExpandableTableViewController: UITableViewController {
+class ProductDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     struct CellData {
         let title: String
@@ -19,30 +19,24 @@ class ExpandableTableViewController: UITableViewController {
         CellData(title: "Cell 1", details: "Details for Cell 1", isExpanded: false),
         CellData(title: "Cell 2", details: "Details for Cell 2", isExpanded: false)
     ]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+
+        // Do any additional setup after loading the view.
     }
     
-    // MARK: - Table view data source
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return data.reduce(0) { $0 + ($1.isExpanded ? 2 : 1) }
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var currentIndex = 0
         for item in data {
             if currentIndex == indexPath.row {
@@ -64,7 +58,7 @@ class ExpandableTableViewController: UITableViewController {
         return UITableViewCell()
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         var currentIndex = 0
         for (index, item) in data.enumerated() {
             if currentIndex == indexPath.row {
@@ -79,33 +73,15 @@ class ExpandableTableViewController: UITableViewController {
             }
         }
     }
-}
 
-import Foundation
+    /*
+    // MARK: - Navigation
 
-class Review {
-    var username: String
-    var rating: Int
-    var comment: String
-
-    init(username: String, rating: Int, comment: String) {
-        self.username = username
-        self.rating = rating
-        self.comment = comment
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-}
+    */
 
-
-class Product {
-    var name: String
-    var description: String
-    var price: Double
-    var reviews: [Review]  // Associated reviews
-
-    init(name: String, description: String, price: Double, reviews: [Review] = []) {
-        self.name = name
-        self.description = description
-        self.price = price
-        self.reviews = reviews
-    }
 }
