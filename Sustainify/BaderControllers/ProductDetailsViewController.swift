5faaa1b924b1eafd5b5d07d755f70843d4971060
BaderEbrahim <xvbader@gmail.com>
2024-12-28 21:21:53 +0300

    update to stuff

--- Sustainify/BaderControllers/ProductDetailsViewController.swift
+++ Sustainify/BaderControllers/ProductDetailsViewController.swift
@@ -7,19 +7,15 @@
 
 import UIKit
 
-class ProductDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate /*UICollectionViewDataSource, UICollectionViewDelegateFlowLayout*/ {
+class ProductDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var descriptionLabel: UILabel!
-    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var quantityStepper: UIStepper!
     @IBOutlet weak var quantityLabel: UILabel!
     @IBOutlet weak var addToCartButton: UIButton!
     
-    @IBOutlet weak var reviewsCollectionView: UICollectionView!
-    
-
-    var expandedStates: [Bool] = []
+    var expandedStates: [Bool] = [] // Keeps track of expanded/collapsed states
     var tags: [Tag] = [] // This will hold the tags for the product
     var selectedQuantity: Int = 0 // Variable to store the quantity
 
@@ -28,7 +24,7 @@ class ProductDetailsViewController: UIViewController, UITableViewDataSource, UIT
 
         // Initialize quantity
         quantityLabel.text = "0"
-        
+
         // Define tags
         let ecoTag = Tag(name: "Eco Score", description: "This product has an eco score based on sustainability.")
         let tag1 = Tag(name: "Organic", description: "This product is made from organic ingredients.")
@@ -44,54 +40,55 @@ class ProductDetailsViewController: UIViewController, UITableViewDataSource, UIT
         tableView.delegate = self
         tableView.dataSource = self
 
-        // Register a custom cell class if necessary
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TagCell")
+        // Register the custom cell
+        //tableView.register(UINib(nibName: "TagsTableViewCell", bundle: nil), forCellReuseIdentifier: "TagsTableViewCell")
 
-        // Allow table view to calculate row height automatically
+        // Enable dynamic row height
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 80
-        
-        // Set the description label text
-        //let product = Product(name: "Sample Product", discription: "this product has been proven to be effective in eliminating curses of grade 1 and even special grade", tags: tags); descriptionLabel.text = product.discription
     }
-
+    
     @IBAction func quantityStepper(_ sender: UIStepper) {
-        // Update label with value
-        quantityLabel.text = Int(sender.value).description
-    }
-
-    @IBAction func addToCartButtonClicked(_ sender: UIButton) {
-        if let quantity = Int(quantityLabel.text ?? "0") {
-            selectedQuantity = quantity
-            print("Quantity added to cart: \(selectedQuantity)")
+            // Update label with value
+            quantityLabel.text = Int(sender.value).description
         }
-    }
 
+        @IBAction func addToCartButtonClicked(_ sender: UIButton) {
+            if let quantity = Int(quantityLabel.text ?? "0") {
+                selectedQuantity = quantity
+                print("Quantity added to cart: \(selectedQuantity)")
+            }
+        }
+
+    // MARK: - UITableViewDataSource Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tags.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TagCell", for: indexPath)
-        let tag = tags[indexPath.row]
-        cell.textLabel?.text = tag.name
-        cell.textLabel?.numberOfLines = 0  // Enable multiline
-
-        if expandedStates[indexPath.row] {
-            cell.detailTextLabel?.text = tag.description
-            cell.detailTextLabel?.numberOfLines = 0  // Enable multiline
-        } else {
-            cell.detailTextLabel?.text = nil
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TagsTableViewCell", for: indexPath) as? TagsTableViewCell else {
+            fatalError("Failed to dequeue TagsTableViewCell")
         }
+
+        // Get the current tag
+        let tag = tags[indexPath.row]
+
+        // Configure the cell
+        cell.tagLabel.text = tag.name
+        cell.infoLabel.text = tag.description
+
+        // Set the visibility of the infoLabel based on the expanded state
+        cell.infoLabel.isHidden = !expandedStates[indexPath.row]
 
         return cell
     }
 
+    // MARK: - UITableViewDelegate Methods
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // Toggle the expanded state of the selected cell
         expandedStates[indexPath.row].toggle()
 
-        // Reload the affected row
+        // Reload the affected row to apply the changes
         tableView.reloadRows(at: [indexPath], with: .automatic)
     }
 }
